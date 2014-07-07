70d799187cb67a220d76a11813f94ff9df96f499
Sahan <keeblersf@gmail.com>
2014-07-07 00:23:25 -0400

    js racer - oojs version

---
+++ README.md
@@ -0,0 +1,13 @@
+Javascript racer v 0.2
+
+To run the game, clone the directory to you local machine and open the racer.html file in your browser.
+Use the "A" key to advance player 1, and "L" key to davance player 2.
+
+
+
+Features for future releases:
+
+Reset game button
+Backend DB to keep score
+Custom Player Names
+Leaderboard
---
+++ start_game.js
@@ -0,0 +1,32 @@
+function startGame (board, player1, player2) {
+
+		var player1_position = player1.position
+		var player2_position = player2.position
+
+	  function update_player_position(player, position){
+		    $("#player"+player+"_strip td").removeClass();
+		    $("#player"+player+"_strip td").eq(position).addClass("active");
+	  };
+
+		$("body").keyup(function(e){
+		  	var keycode = (e.keyCode ? event.keyCode : event.which);
+
+		    if (keycode == 65 && player1_position < board.cells) {
+			      player1_position += 1;
+			      update_player_position(1, player1_position);
+			  }
+
+		    if (keycode == 76 && player2_position < board.cells) {
+			      player2_position += 1;
+			      update_player_position(2, player2_position);
+			  }
+
+		    if (player1_position == board.cells) {
+		    		alert('Player 1 wins');
+		    }
+		    
+		    if (player2_position == board.cells) {
+		      	alert('Player 2 wins');
+		    }
+		});
+}
