325b3870d739e6835e5abf175f8dbeef81b4fe05
Sahan <keeblersf@gmail.com>
2014-07-07 17:53:57 -0400

    fixed var and function naming

--- prepare_game.js
+++ prepare_game.js
@@ -1,13 +1,13 @@
 function prepareGame() {
-    var cellCount = 20
-    var board = new Board(cellCount);
+    var cell_count = 20
+    var board = new drawBoard(cell_count);
     var player1 = new Player('Player1');
     var player2 = new Player('Player2');
-    startGame(cellCount, player1, player2);
+    startGame(cell_count, player1, player2);
 }
 
 
-function Board(cell_count) {
+function drawBoard(cell_count) {
     for(cell=0; cell < cell_count; cell++) {
         $('.active').after('<td></td>');
     };
--- start_game.js
+++ start_game.js
@@ -3,9 +3,9 @@ function startGame (cell_count, player1, player2) {
     var player1_position = player1.position
     var player2_position = player2.position
 
-    function update_player_position(player, position){
-        $("#player"+player+"_strip td").removeClass();
-        $("#player"+player+"_strip td").eq(position).addClass("active");
+    function updatePlayerPosition(player_number, position){
+        $("#player"+player_number+"_strip td").removeClass();
+        $("#player"+player_number+"_strip td").eq(position).addClass("active");
     };
 
     $("body").keyup(function(e){
@@ -13,20 +13,20 @@ function startGame (cell_count, player1, player2) {
 
         if (keycode == 65 && player1_position < cell_count) {
             player1_position += 1;
-            update_player_position(1, player1_position);
+            updatePlayerPosition(1, player1_position);
         }
 
         if (keycode == 76 && player2_position < cell_count) {
             player2_position += 1;
-            update_player_position(2, player2_position);
+            updatePlayerPosition(2, player2_position);
         }
 
         if (player1_position == cell_count) {
-            alert('Player 1 wins');
+            alert(player1.player_name + ' wins');
         }
 	        
         if (player2_position == cell_count) {
-            alert('Player 2 wins');
+            alert(Player2.player_name + ' wins');
         }
     });
 }
