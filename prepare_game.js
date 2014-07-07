9dcc8770513230414f8bc77ec407145dc6c3cfbf
Sahan <keeblersf@gmail.com>
2014-07-07 00:30:07 -0400

    fix indentation

--- prepare_game.js
+++ prepare_game.js
@@ -1,23 +1,23 @@
 function prepareGame() {
-		var board = new drawBoard(20);
-		var player1 = new Player('Player1');
-		var player2 = new Player('Player2');
-		startGame(board, player1, player2);
+    var board = new drawBoard(20);
+    var player1 = new Player('Player1');
+    var player2 = new Player('Player2');
+    startGame(board, player1, player2);
 }
 
 
 function drawBoard(cells) {
-		this.cells = cells
+    this.cells = cells
 
-		for(cell=0; cell < cells; cell++) {
-			$('.active').after('<td></td>');
-		};
+    for(cell=0; cell < cells; cell++) {
+        $('.active').after('<td></td>');
+    };
 }
 
 
 function Player(player_name) {
-		this.player_name = player_name;
-		this.position = 0
+    this.player_name = player_name;
+    this.position = 0
 }
 
 
